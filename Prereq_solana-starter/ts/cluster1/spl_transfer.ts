3162dbc000edde5fac00fd05bbcca9a8184d58bf
Parth Mandawaria <125116214+parthhcodess@users.noreply.github.com>
2025-07-01 23:10:21 +0530

    added week 0 prereqs

---
+++ Prereq_solana-starter/ts/cluster1/spl_transfer.ts
@@ -0,0 +1,49 @@
+import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
+import wallet from "/home/parth/wallet/my_wallet.json"
+import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+
+// We're going to import our keypair from the wallet file
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+//Create a Solana devnet connection
+const commitment: Commitment = "confirmed";
+const connection = new Connection("https://api.devnet.solana.com", commitment);
+
+// Mint address
+const mint = new PublicKey("AaS3mQPS6sScGtJ8ABxvg4vcyWkG1pMtt5y2Uu1BcYuv");
+
+// Recipient address
+const to = new PublicKey("FVdLp9jrbstgPZDqqWuNbSEKURxigGkigFBRmnCAAksh");
+
+(async () => {
+    try {
+        // Get the token account of the fromWallet address, and if it does not exist, create it
+        const fromWallet = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,
+            mint,
+            keypair.publicKey
+        )
+
+        // Get the token account of the toWallet address, and if it does not exist, create it
+        const toWallet = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,
+            mint,
+            to
+        )
+
+        // Transfer the new token to the "toTokenAccount" we just created
+        const toTokenAccount = await transfer(
+            connection,
+            keypair,
+            fromWallet.address,
+            toWallet.address,
+            keypair,
+            100000
+        )
+        console.log("Transaction Signature:", toTokenAccount);
+    } catch(e) {
+        console.error(`Oops, something went wrong: ${e}`)
+    }
+})();
